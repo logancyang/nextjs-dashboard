9b2a30ad7d99b8ee7238ad8ade49cb5179227e79
Logan Yang <logancyang@gmail.com>
2023-12-13 20:36:28 -0800

    Add new pages and layout components

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,4 @@
+// It doesn't matter if it's named Page() or CustomersPage() as long as it's default export
+export default function CustomersPage() {
+  return <p>Customers Page</p>;
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,4 @@
+// It doesn't matter if it's named Page() or InvoicesPage() as long as it's default export
+export default function InvoicesPage() {
+  return <p>Invoices Page</p>;
+}
