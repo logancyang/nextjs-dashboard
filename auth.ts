43af2b28d822a6c7a0235af459289ba34c622896
Logan Yang <logancyang@gmail.com>
2023-12-22 15:10:40 -0800

    Add authentication configuration and middleware

---
+++ auth.ts
@@ -0,0 +1,20 @@
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { authConfig } from './auth.config';
+import { z } from 'zod';
+
+// Good to know:
+// Although we're using the Credentials provider, it's generally recommended to use alternative providers such as OAuth or email providers.
+// See the NextAuth.js docs https://authjs.dev/getting-started/providers for a full list of options.
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+      },
+    }),
+  ],
+});
